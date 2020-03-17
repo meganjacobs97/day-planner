999cb3b16925ace71e2eb717b2c81173ebe1e13d
Megan Jacobs <meganjacobs@live.com>
2020-03-17 16:48:37 -0700

    added pseudocode

---
+++ script.js
@@ -0,0 +1,5 @@
+//retreive time and date 
+//displays day, month day instead <p id="currentDay">
+//ids of 9amTextArea, etc 
+    //add class past, present, or future depending on current hour 
+    //grab saved data and set the text of each 
